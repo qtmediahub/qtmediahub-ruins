qtmediahub/qtmediahub-ruins

commit 5ce3e44e47933dd562f333315f7194472664d815

Weather widget draft

hub/skins/confluence/720p/components/Dialog.qml
@@ -28,10 +28,8 @@ FocusScope {
     opacity: 0; visible: false
     scale: 0
 
-    width: frame.width
-    height: frame.height
-
-    property alias defaultDecoration: frame.visible
+    width: 400
+    height: 200
 
     //useful for focus debugging
     //onActiveFocusChanged: console.log(idtext + " just " + (activeFocus ? "got" : "lost") + " focus")
@@ -72,9 +70,9 @@ FocusScope {
         }
     ]
 
-    Image {
-        id: frame
+    BorderImage {
         source: themeResourcePath + "/media/ContentPanel.png"
+        anchors.fill: parent
     }
 
     function onHideTransitionStarted() {

hub/skins/confluence/720p/Confluence.qml
@@ -99,7 +99,7 @@ FocusScope {
         else if(customCursorLoader.status == Component.Error)
             console.log(customCursorLoader.errorString())
         
-        var weatherDialogLoader = Qt.createComponent("weather/WeatherDialog.qml")
+        var weatherDialogLoader = Qt.createComponent("WeatherDialog.qml")
         if(weatherDialogLoader.status == Component.Ready)
             weatherDialogLoader.createObject(confluence)
         else if(weatherDialogLoader.status == Component.Error)

hub/skins/confluence/720p/WeatherDialog.qml
@@ -1,0 +1,254 @@
+/****************************************************************************
+
+This file is part of the QtMediaHub project on http://www.gitorious.org.
+
+Copyright (c) 2009 Nokia Corporation and/or its subsidiary(-ies).*
+All rights reserved.
+
+Contact:  Nokia Corporation (qt-info@nokia.com)**
+
+You may use this file under the terms of the BSD license as follows:
+
+"Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* Neither the name of Nokia Corporation and its Subsidiary(-ies) nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+
+****************************************************************************/
+
+import QtQuick 1.0
+import "components"
+
+
+FocusScope {
+    id: root
+
+    anchors.fill: parent
+
+    visible: false
+    opacity: 0
+    scale: 0
+    z: 1
+
+    property string city: "munich"
+
+
+    function f2c(f) {
+        return ((f-32)*5/9.0).toFixed(0);
+    }
+
+    Row {
+        anchors.centerIn: parent
+        spacing: 100
+        Dialog {
+            id: dialog1
+            width: 480
+            height: 600
+            anchors.centerIn: undefined
+            state: root.state
+
+            Column {
+                anchors.fill: parent
+                anchors.margins: 30
+                spacing: 5
+                Text {
+                    color: "white"
+                    font.pointSize: 20
+                    anchors.horizontalCenter: parent.horizontalCenter
+                    text: "CURRENT TEMP"
+                }
+
+                Text {
+                    color: "white"
+                    font.pointSize: 20
+                    anchors.horizontalCenter: parent.horizontalCenter
+                    text: weatherModel.count > 0 ? weatherModel.get(0).city : ""
+                }
+
+                Text {
+                    color: "grey"
+                    font.pointSize: 12
+                    anchors.horizontalCenter: parent.horizontalCenter
+                    text: weatherModel.count > 0 ? "Last Updated - " + weatherModel.get(0).current_date_time : ""
+                }
+
+
+                Item {
+                    anchors.horizontalCenter: parent.horizontalCenter
+                    width: parent.width/1.3
+                    height: 200
+                    Text {
+                        id: weatherDegree
+                        color: "white"
+                        font.pointSize: 64
+                        font.bold: true
+                        text: weatherMeasurements.count > 0 ? weatherMeasurements.get(0).temp_c : "0"
+                        anchors.verticalCenter: parent.verticalCenter
+                        anchors.left: parent.left
+                    }
+
+                    Text {
+                        color: "white"
+                        font.pointSize: 20
+                        text: "°C"
+                        anchors.verticalCenter: weatherDegree.top
+                        anchors.left: weatherDegree.right; anchors.leftMargin: 10
+                    }
+
+                    Image {
+                        id: weatherIcon
+                        width: 120
+                        height: width
+                        smooth: true
+                        asynchronous: true
+                        source: weatherMeasurements.count > 0 ? "http://www.google.com" + weatherMeasurements.get(0).icon : ""
+                        anchors.right: parent.right
+                        anchors.verticalCenter: parent.verticalCenter                    }
+
+                }
+
+                Text {
+                    color: "white"
+                    font.pointSize: 20
+                    anchors.horizontalCenter: parent.horizontalCenter
+                    text: weatherMeasurements.count > 0 ? weatherMeasurements.get(0).condition : ""
+                }
+
+                Rectangle {
+                    height: 40
+                    width: 5
+                    color: "transparent"
+
+                }
+
+                Text {
+                    color: "white"
+                    font.pointSize: 20
+                    anchors.horizontalCenter: parent.horizontalCenter
+                    text: weatherMeasurements.count > 0 ? weatherMeasurements.get(0).humidity : ""
+                }
+
+
+                Text {
+                    color: "white"
+                    font.pointSize: 20
+                    anchors.horizontalCenter: parent.horizontalCenter
+                    text: weatherMeasurements.count > 0 ? weatherMeasurements.get(0).wind_condition : ""
+                }
+            }
+        }
+
+         Dialog {
+            id: dialog0
+            width: 480
+            height: 600
+            anchors.centerIn: undefined
+            state: root.state
+
+            Column {
+                anchors.fill: parent
+                anchors.margins: 30
+                spacing: 5
+
+                Text {
+                    color: "white"
+                    font.pointSize: 20
+                    anchors.horizontalCenter: parent.horizontalCenter
+                    text: "WEATHER FORECAST"
+                }
+
+                ListView {
+                    height: 500
+                    width: parent.width
+                    clip: true
+                    model: weatherForecast
+                    delegate: Column {
+                        height: 120
+                        width: 400
+
+                        Text {
+                            color: "white"
+                            font.pointSize: 20
+                            anchors.horizontalCenter: parent.horizontalCenter
+                            text: weatherForecast.count > 0 ? weatherForecast.get(index).day_of_week : ""
+                        }
+                        Row {
+                            //height: parent.height
+                            width: parent.width
+                            spacing: 20
+                            Text {
+                                color: "white"
+                                font.pointSize: 20
+                                text: weatherForecast.count > 0 ? "High: " + root.f2c(weatherForecast.get(index).high_f) + " °C" : ""
+                            }
+
+                            Text {
+                                color: "white"
+                                font.pointSize: 20
+                                text: weatherForecast.count > 0 ? "Low: " + root.f2c(weatherForecast.get(index).low_f)  + " °C" : ""
+                            }
+
+                        }
+                        Text {
+                            color: "white"
+                            font.pointSize: 20
+                            text: weatherForecast.count > 0 ? weatherForecast.get(index).condition : ""
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    XmlListModel {
+        id: weatherModel
+        source: "http://www.google.com/ig/api?weather=" + city
+        query: "/xml_api_reply/weather/forecast_information"
+
+        //forecast information
+        XmlRole { name: "city"; query: "city/@data/string()" }
+        XmlRole { name: "forecast_date"; query: "forecast_date/@data/string()" }
+        XmlRole { name: "current_date_time"; query: "current_date_time/@data/string()" }
+    }
+
+    XmlListModel {
+        id: weatherMeasurements
+        source: "http://www.google.com/ig/api?weather=" + city
+        query: "/xml_api_reply/weather/current_conditions"
+
+        //current condition
+        XmlRole { name: "condition"; query: "condition/@data/string()" }
+        XmlRole { name: "temp_c"; query: "temp_c/@data/string()" }
+        XmlRole { name: "humidity"; query: "humidity/@data/string()" }
+        XmlRole { name: "icon"; query: "icon/@data/string()" }
+        XmlRole { name: "wind_condition"; query: "wind_condition/@data/string()" }
+
+    }
+
+    XmlListModel {
+        id: weatherForecast
+        source: "http://www.google.com/ig/api?weather=" + city
+        query: "/xml_api_reply/weather/forecast_conditions"
+
+        XmlRole { name: "day_of_week"; query: "day_of_week/@data/string()" }
+        XmlRole { name: "low_f"; query: "low/@data/string()" }
+        XmlRole { name: "high_f"; query: "high/@data/string()" }
+        XmlRole { name: "icon"; query: "icon/@data/string()" }
+        XmlRole { name: "condition"; query: "condition/@data/string()" }
+
+    }
+
+    states: State {
+        name: "visible"
+        PropertyChanges {
+            target: root
+            visible: true
+            opacity: 1
+            scale: 1
+        }
+    }
+
+    Engine { name: "Weather"; role: "weather"; visualElement: root }
+}

commit 21f62cf100b4a2a1894b7dba761e7094debc4616

Fix default Dialog geometry broken in 5ce3e44e47933dd562f333315f7194472664d815

hub/skins/confluence/720p/components/Dialog.qml
@@ -28,8 +28,10 @@ FocusScope {
     opacity: 0; visible: false
     scale: 0
 
-    width: 400
-    height: 200
+    width: 1024; height: 512
+
+    property alias defaultDecoration: frame.visible
+
 
     //useful for focus debugging
     //onActiveFocusChanged: console.log(idtext + " just " + (activeFocus ? "got" : "lost") + " focus")
@@ -71,6 +73,7 @@ FocusScope {
     ]
 
     BorderImage {
+        id: frame
         source: themeResourcePath + "/media/ContentPanel.png"
         anchors.fill: parent
     }